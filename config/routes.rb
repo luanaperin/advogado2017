24f8a784bda3449f335940d7ecca4319ed9a214f
Luana Perin <luanakperin@gmail.com>
2017-10-08 17:46:36 -0300

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   root to: 'pages#home'
+
+   get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
