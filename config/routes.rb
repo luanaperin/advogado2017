264cfd7f24e87a6dafa03d4608ef75208994c35e
Luana Perin <luanakperin@gmail.com>
2017-10-09 10:35:13 -0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   root to: 'pages#home'
 
-   get '/contact' => 'pages#contact'
+  resources :users, only: [:create, :index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
